b5e77acb6990b355c7933a807a7b34ed2d0652b1
Jinghan <phantom.there@gmail.com>
2016-08-31 05:44:39 +0800

    intersection

--- SpatialiteTrial/SpatialiteTrial/STDatabaseManager.swift
+++ SpatialiteTrial/SpatialiteTrial/STDatabaseManager.swift
@@ -62,27 +62,39 @@ class STDatabaseManager: NSObject {
 	func getPathsInRegion(region: MKCoordinateRegion, completion: ([STPath] -> Void)){
 		_databaseQueue.inDatabase({
 			database in
-			let xMin = region.center.longitude - region.span.longitudeDelta / 2
-			let yMin = region.center.latitude - region.span.latitudeDelta / 2
-			let xMax = region.center.longitude + region.span.longitudeDelta / 2
-			let yMax = region.center.latitude + region.span.latitudeDelta / 2
+			let xMin = region.center.longitude - region.span.longitudeDelta
+			let yMin = region.center.latitude - region.span.latitudeDelta
+			let xMax = region.center.longitude + region.span.longitudeDelta
+			let yMax = region.center.latitude + region.span.latitudeDelta
+			
+			let tolerance = region.span.longitudeDelta / 50
 			
 			let screenPolygon = "GeomFromText('POLYGON((\(xMin) \(yMin), \(xMin) \(yMax), \(xMax) \(yMax), \(xMax) \(yMin)))')"
-			
-			
-			let querySQL = "SELECT track_id, AsBinary(track_geometry) FROM tracks WHERE MbrOverlaps(track_geometry, " + screenPolygon + ")"
+			let select = "SELECT track_id, AsBinary(Intersection(Simplify(track_geometry, \(tolerance)), " + screenPolygon + ")) FROM tracks "
+			let querySQL = select + "WHERE MbrOverlaps(track_geometry, " + screenPolygon + ") OR MbrContains(track_geometry, " + screenPolygon + ")"
 			let results = self._database.executeQuery(querySQL, withArgumentsInArray: nil)!
 			
 			var paths = [STPath]()
 			
-			while (results.next() && results.columnNameToIndexMap.count != 0) {
-				let data = results.dataForColumn("asbinary(track_geometry)")
-				let reader = WKBByteReader(data: data)
-				reader.byteOrder = Int(CFByteOrderBigEndian.rawValue)
-				
-				let geometry = WKBGeometryReader.readGeometryWithReader(reader) as! WKBLineString
-				let path = STPath(lineString: geometry)
-				paths.append(path)
+			while (results.next()) {
+				if results.hasAnotherRow() {
+					if let data = results.dataForColumnIndex(1) {
+						let reader = WKBByteReader(data: data)
+						reader.byteOrder = Int(CFByteOrderBigEndian.rawValue)
+						let geometry = WKBGeometryReader.readGeometryWithReader(reader)
+						
+						if let lineString = geometry as? WKBLineString {
+							let path = STPath(lineString: lineString)
+							paths.append(path)
+						} else if let multiLineString = geometry as? WKBMultiLineString {
+							print("multiline")
+							for lineString in multiLineString.getLineStrings() {
+								let path = STPath(lineString: lineString as! WKBLineString)
+								paths.append(path)
+							}
+						}
+					}
+				}
 			}
 			
 			completion(paths)
